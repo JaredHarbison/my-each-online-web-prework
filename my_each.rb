a877dcc3c865082bbf19a52419d58052b9144c20
Jared Harbison <jared.harbison@gmail.com>
2019-01-07 17:43:16 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -2,14 +2,12 @@
 def my_each(array)
   if block_given?
     i = 0
- 
-    while i < array.length
-      yield(array[i])
-      i = i + 1
+  while i < array.length
+    yield(array[i])
+    i = i + 1
     end
- 
     array
   else
-     "Hey! No collection was given!"
+    "Hey! No collection was given!"
   end
 end
